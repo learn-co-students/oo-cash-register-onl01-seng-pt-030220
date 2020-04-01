2ce50c605555c21861f5ef04c09d8699c911e099
Craig Storer <craigstorer@gmail.com>
2020-04-01 12:20:30 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,21 @@
+class CashRegister
+  
+  attr_accessor :employee_discount
+  
+  def initialize(employee_discount = nil)
+    @total = 0
+    @employee_discount = employee_discount
+  end
+  
+  # def discount
+  #   @discount  = discount
+  # end
+  
+#     def discount(number)
+#     self.to_f / number.to_f * 100.0
+#   end
+# end
 
+ 
+  
+end
